1e14a35d44a58ed62af4c54bb02845ec51e8d753
CarlyGrass <carly.grass@snhu.edu>
2021-09-24 13:11:06 -0400

    clean up code

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,8 @@
-from django.db import models
-from django.db import models
 
-class Post(models.Model):
-    text = models.TextField()
+from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
 
     def __str__(self):
         return self.text[:50]
-
-# Create your models here.
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
@@ -6,5 +6,3 @@ class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
-
-# Create your views here.
