4a4f31a6fd771f1babe3f794b1471238ca6f3daa
CarlyGrass <carly.grass@snhu.edu>
2021-09-29 12:47:17 -0400

    clean up code

--- posts/tests.py
+++ posts/tests.py
@@ -7,9 +7,10 @@ class PostModelTest(TestCase):
         Post.objects.create(text='just a test')
 
     def test_text_content(self):
-        post=Post.objects.get(id=1)
+        post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
+
 class HomePageViewTest(TestCase):
 
     def setUp(self):
@@ -23,5 +24,3 @@ class HomePageViewTest(TestCase):
         resp = self.client.get(reverse('home'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'home.html')
-
-# Create your tests here.
